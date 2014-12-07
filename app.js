35d79d62df18a498f1ee2baf65d202ff56002a1b
Alexander Gottfried <alexander.gottfried89@gmail.com>
2014-12-07 17:31:35 +0100

    Updated app.js
    Local server port

--- app.js
+++ app.js
@@ -20,17 +20,18 @@ function (http, module, path, express, db, favicon, logger, bodyParser, methodOv
 
     // Database
     // default to a 'localhost' configuration:
-    var connection_string = '127.0.0.1:27017/LazyNetwork';
+    var connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + "LazyNetwork";
     // if OPENSHIFT env variables are present, use the available connection info:
    if(process.env.OPENSHIFT_MONGODB_DB_URL){
       connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + "LazyNetwork";
     }
+	console.log(process.env);
 	console.log(connection_string);
     db.connect(connection_string);
 
     // all environments
     var dirname = path.dirname(module.uri);
-    app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+    app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
     app.set('server_ip_address', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
     app.set('views', path.join(dirname, 'views'));
     app.set('view engine', 'jade');
